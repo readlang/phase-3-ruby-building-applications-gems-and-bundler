89860754117fc62971d992ea6a3c16010b8367b9
Read Langworthy <rlangworthy@gmail.com>
2022-02-23 15:10:02 -0800

    commit tree e9d38bd0c39adb1f7e52d75f7c26dd72ccdaf954

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,7 @@
 # Set up Bundler here following the instructions in the README
+
+require 'bundler/setup'
+Bundler.require(:default, :development)
+
+require_relative "../spec/bundler_spec"
+require_relative "../spec/spec_helper"
